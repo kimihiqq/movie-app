5d7fa04dd02ba7ab6a78b197f5a3912ffbbec601
kimihiqq <kimihiqq@gmail.com>
2023-07-13 16:16:15 +0900

    Updates

---
+++ precache-manifest.511d201b9a033a11913088da845a5eaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be57201fec1e43f52dfa43f063876073",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "9188062fcdd7e17b5a71",
+    "url": "/movie_app/static/css/main.7a70783e.chunk.css"
+  },
+  {
+    "revision": "68747541c478608300e0",
+    "url": "/movie_app/static/js/2.4c3191cb.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/movie_app/static/js/2.4c3191cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9188062fcdd7e17b5a71",
+    "url": "/movie_app/static/js/main.e01a3aa9.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/movie_app/static/js/main.e01a3aa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d94f6fb6f94a15669d3",
+    "url": "/movie_app/static/js/runtime-main.5a7bb52e.js"
+  }
+]);
